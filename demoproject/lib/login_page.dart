0809a35d97ccaffb18426af1f60836c5179800c0
Tayyaba <tayybaajmal007@gmail.com>
2022-12-12 18:21:50 +0500

    Day4completed

--- demoproject/lib/login_page.dart
+++ demoproject/lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class loginPage extends StatelessWidget {
@@ -6,16 +8,53 @@ class loginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text ("login page",
-        style:TextStyle(
-          fontSize : 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-
-        ),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/undraw_Mobile_login_re_9ntv.png",
+            fit: BoxFit.cover,
+          ),
+          Text(
+            "Wellcome to my app",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "enter Username",
+                    labelText: "username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "enter password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text("login"),
+                  onPressed: () {
+                    print("Hi to tayyab's app");
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-    ),
     );
   }
 }
--- demoproject/lib/main.dart
+++ demoproject/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:demoproject/Home_page.dart';
 import 'package:demoproject/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart'; 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,10 +15,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      primaryTextTheme:GoogleFonts.latoTextTheme()
       ),
-      initialRoute: "/home",
+      darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
 
         "/": (context) => loginPage(),
